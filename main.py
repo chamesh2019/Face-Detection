3a6bd6ca9faa06b6ee29930cfa35ad5233049f0e
Chames Dinuka <chamesh2020@gmail.com>
2024-12-14 23:52:55 +0530

    optimize code

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 cap = cv2.VideoCapture(0)
 
 def main():
-    while True:
+    while cap.isOpened():
         ret, img = cap.read()
+        if not ret:
+            break
+
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4)
+
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.imshow("img", img)
+
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
